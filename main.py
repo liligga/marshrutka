9e47715b9a11a6e27706bcdebcc0450c0d80aed9
Igor <8luxfshdnlz1@mail.ru>
2022-07-28 12:58:44 +0600

    исправления

--- main.py
+++ main.py
@@ -9,23 +9,23 @@ app = FastAPI()
 
 
 @app.get("/")
-def drivers_list():
+async def drivers_list():
     """Показываем всех водителей"""
     return drivers
 
 
 @app.get("/drivers")
-def driver_by_bus(bus: int = 1):
+async def driver_by_bus(bus: int = 1):
     """Показываем всех водителей, которые работают
     на одном маршруте
-    запрос будет выглядеть /driver?bus=3
+    запрос будет выглядеть /drivers?bus=3
     """
     return list(filter(lambda d: d.get('bus') == bus, drivers))
 
 
 @app.get("/drivers/{driver_id}")
-def driver_details(driver_id: int):
-    """Показываем одного водителя, по id"""
+async def driver_details(driver_id: int):
+    """Показываем одного водителя, по id, id передается в пути"""
     try:
         driver = list(filter(lambda d: d.get('id') == driver_id, drivers))[0]
     except IndexError:
@@ -35,8 +35,10 @@ def driver_details(driver_id: int):
 
 
 @app.put("/drivers")
-def update_coordinates(driver: Driver_Pydantic):
-    """меняем координаты одного водителя, все данные в теле запорса"""
+async def update_coordinates(driver: Driver_Pydantic):
+    """меняем координаты одного водителя, все данные(в том числе id)
+    в теле запорса."""
+    # Чтобы получить значение например поля id - обращаемся к driver.id
     try:
         driver_obj = list(filter(lambda d: d.get('id') == driver.id, drivers))[0]
         driver_obj['lat'] = driver.lat
